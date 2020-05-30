b5c6162c2bf00532d6494840de016e75be4a9ddf
Alex Erdei <mralexerdei@yahoo.co.uk>
2020-05-30 13:58:40 +0100

    add render() function

--- library.js
+++ library.js
@@ -22,9 +22,42 @@ function addBookToLibrary(title, author, pages, isRead){
     myLibrary.push(newbook);
 }
 
+function newLineToHTML(book, index){
+    const table = document.querySelector('table');
+    const className = 'line-' + index.toString();
+    const tr = document.createElement('tr');
+    const td1 = document.createElement('td');
+    const td2 = document.createElement('td');
+    const td3 = document.createElement('td');
+    const td4 = document.createElement('td');
+    const td5 = document.createElement('td');
+
+    td1.textContent = book.title;
+    td2.textContent = book.author;
+    td3.textContent = book.pages;
+    if (book.isRead) {
+        td4.textContent = 'Yes';
+    } else {
+        td4.textContent = 'No';
+    }
+
+    tr.classList.add(className);
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    tr.appendChild(td4);
+    tr.appendChild(td5);
+    table.appendChild(tr);
+}
+
+function render(){
+    for (let i = 0; i < myLibrary.length; i++) {
+        newLineToHTML(myLibrary[i], i);
+        console.log(myLibrary[i].info());
+    }
+}
+
 addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, false);
 addBookToLibrary('Classical Mechanics', 'John Taylor', 786, true);
 addBookToLibrary('Mathematical methodes in the physical sciences', 'Mary L. Boas', 793, true);
-console.log(myLibrary[0]);
-console.log(myLibrary[1]);
-console.log(myLibrary[2]);
+render();
